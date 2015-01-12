be9d0cce0e3b5936aa539004c48e8d1380c4a281
Johannes Ewald <johannes.ewald@peerigon.com>
2015-01-12 15:51:10 +0100

    Fix wrong usage of fs.linkSync()

--- linkPackage.js
+++ linkPackage.js
@@ -6,15 +6,15 @@ var fs = require("fs");
 function linkPackage() {
     var packageName = require("../../package.json").name;
     var nodeModules = path.resolve(__dirname, "../");
-    var srcPath = path.join(nodeModules, packageName);
-    var dstPath = path.resolve(__dirname, "../../");
+    var dstPath = path.join(nodeModules, packageName);
+    var srcPath = path.resolve(__dirname, "../../");
 
-    if (fs.existsSync(srcPath)) {
-        if (fs.realpathSync(srcPath) !== dstPath) {
+    if (fs.existsSync(dstPath)) {
+        if (fs.realpathSync(dstPath) !== srcPath) {
             throw new Error("There is already another package '" + packageName + "' installed");
         }
     } else {
-        fs.linkSync(srcPath, dstPath);
+        fs.linkSync(dstPath, srcPath);
     }
 }
 
