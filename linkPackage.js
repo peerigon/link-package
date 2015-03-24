e37d2fd5e9fdf10856e108854afad0d0d281ca9d
Johannes Ewald <johannes.ewald@peerigon.com>
2015-03-24 14:12:21 +0100

    Add comments

--- linkPackage.js
+++ linkPackage.js
@@ -3,6 +3,14 @@
 var path = require("path");
 var fs = require("fs");
 
+/**
+ * Creates a symlink to the specified folderName under the given alias.
+ * When nothing is specified, this function creates a symlink to the project's root folder
+ * with name as specified in the package.json.
+ *
+ * @param {string=} folderName defaults to the project's root folder
+ * @param {string=} alias defaults to the package.json's name attribute
+ */
 function linkPackage(folderName, alias) {
     var packageName = alias || require("../../package.json").name;
     var nodeModules = path.resolve(__dirname, "../");
